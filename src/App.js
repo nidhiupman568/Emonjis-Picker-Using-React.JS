e82d8822f73fb2635c797951ba4b99cf78831647
nidhiupman568 <upmanyunidhi3092@gmail.com>
2024-07-16 22:55:53 +0530

    do something creative

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Filename - App.js
+ 
+import React, { useState } from "react";
+import Picker from "emoji-picker-react";
+ 
+export default function Emoji() {
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+ 
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        console.log(emojiObject.target);
+    };
+ 
+    return (
+        <div>
+            <h3>GeeksforGeeks Emoji Picker</h3>
+            {chosenEmoji ? (
+                <span>
+                    Your Emoji:
+                    <img
+                        style={{ width: "15px" }}
+                        src={chosenEmoji.target.src}
+                    />
+                </span>
+            ) : (
+                <span>No Emoji</span>
+            )}
+            <Picker onEmojiClick={onEmojiClick} />
+        </div>
+    );
 }
-
-export default App;
